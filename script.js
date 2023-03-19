ef5634bccffceaec7d6575dac9b3680e9c8f7386
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-03-19 09:01:08 +0300

    Homework done

---
+++ script.js
@@ -0,0 +1,53 @@
+const post = document.querySelector('.post');
+const title = document.querySelector('.input__name');
+const text = document.querySelector('.input__text');
+const button = document.querySelector('.input__button');
+
+//функция для создания шаблона поста
+
+function makeTemplate(title, body) {
+    let template = '';
+    template = `
+    <div class = 'post__title'>${title}</div>
+    <div class = 'post__body'>${body}</div>
+`
+    post.innerHTML += template;
+
+}
+
+
+//функция для создания поста
+
+function makePost() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: title.value,
+            body: text.value
+        }),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            makeTemplate(data.title, data.body);
+
+        })
+}
+
+//функция для удаления предыдущего поста
+
+function removePost() {
+    post.innerHTML = '';
+}
+
+//создание поста при клике на кнопку и удаление предыдущего
+button.addEventListener('click', () => {
+    makePost();
+    removePost();
+});
+
+
+
+
